d13abe16604a4fede7dda8c87d5a8becb1f200f5
Jonathan <jonathan@mullvad.net>
2022-05-12 13:18:33 +0200

    Add whitelist commit and another lockfile
    Add a starting whitelisted commit that is the beginning commit for when
    we start verifying signatures. This is because this policy has not
    been in place before this one so all commits relating to lockfiles could
    be unsigned.

--- ci/verify-lockfile-signature.sh
+++ ci/verify-lockfile-signature.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 set -eu
-LOCKFILE="../Cargo.lock"
+shopt -s nullglob
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOCKFILES="$SCRIPT_DIR/../Cargo.lock $SCRIPT_DIR/../gui/package-lock.json"
+# The policy of enforcing lockfiles to be signed was not in place before this commit and as such some of the commits before are not signed
+WHITELIST_COMMIT="bdf327cfa"
 
-lockfile_commit_hashes=$(git log --oneline --follow $LOCKFILE | awk '{print $1}')
-unsigned_commits_exist="false"
-for commit in $lockfile_commit_hashes;
+for LOCKFILE in $LOCKFILES;
 do
-    if ! $(git verify-commit $commit 2> /dev/null); then
-        echo Commit $commit is not signed
-        unsigned_commits_exist="true"
-    fi
+    lockfile_commit_hashes=$(git log --oneline $WHITELIST_COMMIT..HEAD --follow $LOCKFILE | awk '{print $1}')
+    unsigned_commits_exist="false"
+    for commit in $lockfile_commit_hashes;
+    do
+        if ! $(git verify-commit $commit 2> /dev/null); then
+            echo Commit $commit is not signed
+            unsigned_commits_exist="true"
+        fi
+    done
 done
 
 if [[ $unsigned_commits_exist == "true" ]]; then
-    echo "true"
+    echo "Unsigned commits exist"
     exit 1
 fi
