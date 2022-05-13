d47d65c71e4532a7c9525b17e65816f9e8d160e5
Jonathan <jonathan@mullvad.net>
2022-05-13 10:33:01 +0200

    Refactor verification script and add github action
    Remove some unnecessary parts from the verification script and clean it
    up. Add a simple github workflow.

--- ci/verify-lockfile-signature.sh
+++ ci/verify-lockfile-signature.sh
@@ -6,20 +6,17 @@ LOCKFILES="$SCRIPT_DIR/../Cargo.lock $SCRIPT_DIR/../gui/package-lock.json"
 # The policy of enforcing lockfiles to be signed was not in place before this commit and as such some of the commits before are not signed
 WHITELIST_COMMIT="bdf327cfa"
 
+unsigned_commits_exist=0
 for LOCKFILE in $LOCKFILES;
 do
     lockfile_commit_hashes=$(git log --oneline $WHITELIST_COMMIT..HEAD --follow $LOCKFILE | awk '{print $1}')
-    unsigned_commits_exist="false"
     for commit in $lockfile_commit_hashes;
     do
         if ! $(git verify-commit $commit 2> /dev/null); then
             echo Commit $commit is not signed
-            unsigned_commits_exist="true"
+            unsigned_commits_exist=1
         fi
     done
 done
 
-if [[ $unsigned_commits_exist == "true" ]]; then
-    echo "Unsigned commits exist"
-    exit 1
-fi
+exit $unsigned_commits_exist
